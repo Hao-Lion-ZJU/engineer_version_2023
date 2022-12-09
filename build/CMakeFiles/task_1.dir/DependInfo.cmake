27577f6ac903aee63b923395b586505108801d72
haolion <haoion_zju@foxmail.com>
2022-12-09 15:46:07 +0800

    first commit

---
+++ build/CMakeFiles/task_1.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/haolion/test/rm/engineer_version/alogrithm/alogrithm.cpp" "/home/haolion/test/rm/engineer_version/build/CMakeFiles/task_1.dir/alogrithm/alogrithm.cpp.o"
+  "/home/haolion/test/rm/engineer_version/main.cpp" "/home/haolion/test/rm/engineer_version/build/CMakeFiles/task_1.dir/main.cpp.o"
+  "/home/haolion/test/rm/engineer_version/robot.cpp" "/home/haolion/test/rm/engineer_version/build/CMakeFiles/task_1.dir/robot.cpp.o"
+  "/home/haolion/test/rm/engineer_version/utils/params.cpp" "/home/haolion/test/rm/engineer_version/build/CMakeFiles/task_1.dir/utils/params.cpp.o"
+  "/home/haolion/test/rm/engineer_version/utils/record.cpp" "/home/haolion/test/rm/engineer_version/build/CMakeFiles/task_1.dir/utils/record.cpp.o"
+  "/home/haolion/test/rm/engineer_version/utils/utils.cpp" "/home/haolion/test/rm/engineer_version/build/CMakeFiles/task_1.dir/utils/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../$(DIR_INCS)"
+  "../utils"
+  "/usr/local/include/opencv4"
+  "/usr/include/eigen3"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/task_1.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task_1.dir/main.cpp.o"
+  "CMakeFiles/task_1.dir/robot.cpp.o"
+  "CMakeFiles/task_1.dir/alogrithm/alogrithm.cpp.o"
+  "CMakeFiles/task_1.dir/utils/record.cpp.o"
+  "CMakeFiles/task_1.dir/utils/params.cpp.o"
+  "CMakeFiles/task_1.dir/utils/utils.cpp.o"
+  "task_1.pdb"
+  "task_1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
